941b0e51ab4379a8b8eab4c693a1af87a8b89a7d
Christian Carnevale <cncarnevale@gmail.com>
2022-05-03 14:34:28 -0400

    kept original connection.js file

--- config/connection.js
+++ config/connection.js
@@ -7,10 +7,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    'theorist_db',
-    'root',
-    'sql1',
-  
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
       host: 'localhost',
       dialect: 'mysql',
