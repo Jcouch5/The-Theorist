80b2aca435d190d3ccb22328bcf37629aceb933d
Devyn Blackstone <devynblackmon@gmail.com>
2022-05-03 10:19:46 -0500

    main.handlebars

---
+++ public/js/logout.js
@@ -0,0 +1,18 @@
+const logout = async () => {
+
+    const response = await fetch('', {
+        method: 'POST',
+        headers: { 'Content-Type' : 'application/json'},
+    });
+
+    if(response.ok) {
+        document.location.replace('/login');
+    }
+    else {
+        alert(response.statusText);
+    }
+};
+
+document
+.querySelector('#logout')
+.addEventListener('click', logout);
