df260bb7cbf2b85a399d964ea9d208e0502699d3
jcouch <jasoncouchjr410@gmail.com>
2022-05-04 12:14:34 -0400

    update

--- public/js/index.js
+++ public/js/index.js
@@ -23,3 +23,52 @@ const signupFormHandler = async (event) => {
 document
   .querySelector('.signup-form')
   .addEventListener('submit', signupFormHandler);
+
+const postFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#new-title').value.trim();
+  const summary = document.querySelector('#new-summary').value.trim();
+
+  if (title && summary) {
+    const response = await fetch('/api/posts', {
+      method: 'POST',
+      body: JSON.stringify({ title, summary }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/posts');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector('.post-form')
+  .addEventListener('submit', postFormHandler);
+
+const commentFormHandler = async (event) => {
+  event.preventDefault();
+
+  const comment = document.querySelector('#new-comment').value.trim();
+
+  if (comment) {
+    const response = await fetch('/api/comment', {
+      method: 'POST',
+      body: JSON.stringify({ comment }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/posts');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector('.comment-form')
+  .addEventListener('submit', commentFormHandler);
