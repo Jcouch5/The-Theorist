7284aa3090d2ff6cef5084a084019256c0612c2d
jcouch <jasoncouchjr410@gmail.com>
2022-05-05 14:15:37 -0400

    moved validator

--- controllers/api/signup.js
+++ controllers/api/signup.js
@@ -1,19 +1,22 @@
 const router = require('express').Router();
+const validator = require('validator');
 const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
   try {
-    const dbUserData = await User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
+    if (validator.isEmail(req.body.email)) {
+      const dbUserData = await User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+      });
 
-    req.session.save(() => {
-      req.session.loggedIn = true;
+      req.session.save(() => {
+        req.session.loggedIn = true;
 
-      res.status(200).json(dbUserData);
-    });
+        res.status(200).json(dbUserData);
+      });
+    }
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
