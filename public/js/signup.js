fd598ad09bbf4b3b53df86bfdaa071a38692152d
jcouch <jasoncouchjr410@gmail.com>
2022-05-05 20:11:37 -0400

    fixed login and signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,7 +4,6 @@ const signupFormHandler = async (event) => {
   const username = document.querySelector('#username-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
-
   if (username && email && password) {
     const response = await fetch('/api/signup', {
       method: 'POST',
